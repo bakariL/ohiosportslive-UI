7a3b8a8c8b364e075110957590fb94462ae003c4
bakariL <bakarilewis@gmail.com>
2023-06-08 00:14:54 -0400

    top banner wip

--- src/app/shared/games-top-banner/upcoming-game-top-ddl/upcoming-game-top-ddl.component.ts
+++ src/app/shared/games-top-banner/upcoming-game-top-ddl/upcoming-game-top-ddl.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upcoming-game-top-ddl.component.css']
 })
 export class UpcomingGameTopDdlComponent implements OnInit {
+  selectedValue: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  onSelectionChange(event: any) {
+    this.selectedValue = event.value;
   }
 
 }
