87217f45dc562fb6dde2dd72070acf65ee0c8199
bakariL <bakarilewis@gmail.com>
2023-06-10 00:22:05 -0400

    livescores

---
+++ src/app/features/live-scores/live-scores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'osl-live-scores',
+  templateUrl: './live-scores.component.html',
+  styleUrls: ['./live-scores.component.css']
+})
+export class LiveScoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
