b614d9e8efc02398bcdaf42566929c8c82530a99
bakariL <bakarilewis@gmail.com>
2023-07-29 16:10:09 -0400

    cleaning up styles 2

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'navbar',
+  selector: 'osl-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+
+  isNavbarOpen = false;
   constructor() {}
 
   ngOnInit(): void {}
+
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+}
+
+toggleNavbar() {
+  this.isNavbarOpen = !this.isNavbarOpen;
+}
+
 }
