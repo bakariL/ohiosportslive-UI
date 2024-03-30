509a7ca4fcb71519315d7238acf10b1aeb5a330e
bakariL <bakarilewis@gmail.com>
2024-03-30 12:02:45 -0400

    asdf

---
+++ src/app/features/live-streaming/container/homepage-livestream-games/homepage-livestream-games.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'osl-home-livestreaming-games',
+  templateUrl: './homepage-livestream-games.component.html',
+  styleUrls: ['./homepage-livestream-games.component.css']
+})
+export class HomeLivestreamingGamesComponent implements OnInit {
+  ngOnInit(): void {}
+
+
+  livestreamingGames: any[] = [
+    // Array of game data
+    { scoreA: 52, scoreB: 61, teamA: 'Team A', teamB: 'Team B', image1: 'path/to/your/image1.jpg', image2: 'path/to/your/image2.jpg', quarter: 'Q3' },
+    // Add more game data as needed
+  ];
+}
