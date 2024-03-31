aabcee0e19ff1c8a5c857fc99b2b78e6c9377dd9
bakariL <bakarilewis@gmail.com>
2024-03-31 18:15:26 -0400

    working so far still working on homepage

--- src/app/features/live-streaming/container/homepage-livestream-games/homepage-livestream-games.component.ts
+++ src/app/features/live-streaming/container/homepage-livestream-games/homepage-livestream-games.component.ts
@@ -11,7 +11,7 @@ export class HomeLivestreamingGamesComponent implements OnInit {
 
   livestreamingGames: any[] = [
     // Array of game data
-    { scoreA: 52, scoreB: 61, teamA: 'Team A', teamB: 'Team B', image1: 'path/to/your/image1.jpg', image2: 'path/to/your/image2.jpg', quarter: 'Q3' },
+    { scoreA: 52, scoreB: 61, teamA: 'Team A', teamB: 'Team B', image1: 'path/to/your/image1.jpg', image2: 'path/to/your/image2.jpg', quarter: '3' },
     // Add more game data as needed
   ];
 }
